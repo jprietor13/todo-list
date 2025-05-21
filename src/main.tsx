d59a7e69224ea4efc19826422ccd5fdcb6a9d136
Juan Prieto <jp1739@gmail.com>
2025-05-21 11:22:21 -0500

    Preparar deploy a GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
